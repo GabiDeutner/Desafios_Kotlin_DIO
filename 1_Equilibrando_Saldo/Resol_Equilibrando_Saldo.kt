4e11621bb42c0b32bac23eee3e1a1fc0ff60b479
Gabriela Paulini Deutner <deutnerg@gmail.com>
2023-08-22 21:42:30 -0300

    Update Resol_Equilibrando_Saldo.kt

--- 1_Equilibrando_Saldo/Resol_Equilibrando_Saldo.kt
+++ 1_Equilibrando_Saldo/Resol_Equilibrando_Saldo.kt
@@ -1,10 +1,15 @@
 fun main() {
+  
     val saldoAtual = readLine()!!.toFloat()
     val valorDeposito = readLine()!!.toFloat()
     val valorRetirada = readLine()!!.toFloat()
 
     //TODO: Calcular o saldo atualizado de acordo com a descrição deste desafio.
+
     val saldoFinal = saldoAtual + valorDeposito - valorRetirada
+
   //TODO: Imprimir o a saída de conforme a tabela de exemplos (uma casa decimal).
+
     print("Saldo atualizado na conta: $saldoFinal")
+
 }
