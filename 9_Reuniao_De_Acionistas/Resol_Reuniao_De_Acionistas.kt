0db865bb8ab3a2ffdfef418fd7a65f32597b4d5b
Gabriela Paulini Deutner <deutnerg@gmail.com>
2023-08-31 18:25:12 -0300

    Update Resol_Reuniao_De_Acionistas.kt

--- 9_Reuniao_De_Acionistas/Resol_Reuniao_De_Acionistas.kt
+++ 9_Reuniao_De_Acionistas/Resol_Reuniao_De_Acionistas.kt
@@ -1,38 +1,45 @@
-Desafios Kotlin: Reunião de Acionistas
-Reunião de Acionistas: Consultando Análises Bancárias
+import java.text.SimpleDateFormat
+import java.util.*
 
-Descrição
-Durante uma reunião de acionistas, alguns pontos importantes são discutidos e com isso surge a necessidade do envolvimento da equipe de desenvolvedores. Com isso, você foi designado para criar uma interface que permitirá aos acionistas de uma empresa acessarem informações relevantes sobre as análises da organização bancária. Sua tarefa é implementar uma classe que representa essa interface e que fornece um método para consultar as análises de desempenho e outros tipos dentro de um determinado período.
+fun main(args: Array<String>) {
+    val scanner = Scanner(System.`in`)
+    val dataInicial = scanner.nextLine()
+    val dataFinal = scanner.nextLine()
 
-Requisitos:
+    val sistemaAcionistas = SistemaAcionistas()
+    val analises = sistemaAcionistas.obterAnalisesDesempenho(dataInicial, dataFinal)
 
-A classe deve ter um método chamado obterAnalisesDesempenho que recebe como parâmetros uma data inicial e uma data final.
+    for (analise in analises) {
+        println(analise)
+    }
+}
 
-Entrada
-As datas inicial e final, no formato "dd/mm/aaaa", representando o intervalo de tempo desejado para consulta.
+class SistemaAcionistas {
+    fun obterAnalisesDesempenho(dataInicialStr: String, dataFinalStr: String): List<String> {
+        val df = SimpleDateFormat("dd/MM/yyyy")
+        val dataInicial = df.parse(dataInicialStr)
+        val dataFinal = df.parse(dataFinalStr)
 
-Saída
-Uma lista contendo as análises de desempenho financeiro realizadas dentro do período especificado.
+        val analises = mutableListOf<Analise>()
+        analises.add(Analise(df.parse("01/01/2023"), "Analise de Desempenho Financeiro"))
+        analises.add(Analise(df.parse("15/02/2023"), "Analise de Riscos e Exposicoes"))
+        analises.add(Analise(df.parse("31/03/2023"), "Analises Corporativas"))
+        analises.add(Analise(df.parse("01/04/2023"), "Analise de Politicas e Regulamentacoes"))
+        analises.add(Analise(df.parse("15/05/2023"), "Analise de Ativos"))
+        analises.add(Analise(df.parse("30/06/2023"), "Analise de Inovacao e Tecnologia"))
 
-Entrada:
-01/04/2023
-20/05/2023	
+        //TODO: Implemente o filtro das análises dentro do período especificado. Dica: Crie uma lista para armazenar as análises filtradas e use um loop for para filtrar as análises.
+        val analisesFiltradas = mutableListOf<String>()
+        for (analise in analises) {
+            if (analise.data in dataInicial..dataFinal) {
+                analisesFiltradas.add(analise.descricao)
+            }
+        }
 
-Saída:
-Analise de Politicas e Regulamentacoes
-Analise de Ativos
+        // Retornar a lista de análises filtradas
+        return analisesFiltradas
+    }
+    // TODO: Retorne a lista de análises filtradas.
+}
 
-Entrada:
-05/03/2023
-05/04/2023	
-
-Saída:
-Analises Comporativas
-Analise de Politicas e Regulamentacoes
-
-Entrada:
-05/01/2023
-03/03/2023	
-
-Saída:
-Analise de Riscos e Exposicoes
+data class Analise(val data: Date, val descricao: String)
