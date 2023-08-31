a7d11019fc6060b0ee7e97a2c6650a06992fe808
Gabriela Paulini Deutner <deutnerg@gmail.com>
2023-08-31 19:32:46 -0300

    Update Resol_Organizando_Ativos.kt

--- 2_Organizando_Seus_Ativos/Resol_Organizando_Ativos.kt
+++ 2_Organizando_Seus_Ativos/Resol_Organizando_Ativos.kt
@@ -10,14 +10,10 @@ fun main() {
         ativos.add(codigoAtivo)
     }
 
-     //TODO: Ordenar os ativos em ordem alfabética.
-     
-    val ativosOrdenados = ativos.sorted()
-
+    //TODO: Ordenar os ativos em ordem alfabética.
+    ativos.sort()
     //TODO: Imprimir a lista de ativos ordenada, conforme a tabela de exemplos.
-    
-    for (ativo in ativosOrdenados) {
-        println(ativo)
+    for (i in ativos){
+        println(i)
     }
-    
 }
