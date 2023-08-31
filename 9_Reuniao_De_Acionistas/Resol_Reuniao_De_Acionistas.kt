b7f2b0bc49c60575b4e95f87fffe8081916cb25b
Gabriela Paulini Deutner <deutnerg@gmail.com>
2023-08-31 17:47:19 -0300

    Create Resol_Reuniao_De_Acionistas.kt

---
+++ 9_Reuniao_De_Acionistas/Resol_Reuniao_De_Acionistas.kt
@@ -0,0 +1,38 @@
+Desafios Kotlin: Reunião de Acionistas
+Reunião de Acionistas: Consultando Análises Bancárias
+
+Descrição
+Durante uma reunião de acionistas, alguns pontos importantes são discutidos e com isso surge a necessidade do envolvimento da equipe de desenvolvedores. Com isso, você foi designado para criar uma interface que permitirá aos acionistas de uma empresa acessarem informações relevantes sobre as análises da organização bancária. Sua tarefa é implementar uma classe que representa essa interface e que fornece um método para consultar as análises de desempenho e outros tipos dentro de um determinado período.
+
+Requisitos:
+
+A classe deve ter um método chamado obterAnalisesDesempenho que recebe como parâmetros uma data inicial e uma data final.
+
+Entrada
+As datas inicial e final, no formato "dd/mm/aaaa", representando o intervalo de tempo desejado para consulta.
+
+Saída
+Uma lista contendo as análises de desempenho financeiro realizadas dentro do período especificado.
+
+Entrada:
+01/04/2023
+20/05/2023	
+
+Saída:
+Analise de Politicas e Regulamentacoes
+Analise de Ativos
+
+Entrada:
+05/03/2023
+05/04/2023	
+
+Saída:
+Analises Comporativas
+Analise de Politicas e Regulamentacoes
+
+Entrada:
+05/01/2023
+03/03/2023	
+
+Saída:
+Analise de Riscos e Exposicoes
