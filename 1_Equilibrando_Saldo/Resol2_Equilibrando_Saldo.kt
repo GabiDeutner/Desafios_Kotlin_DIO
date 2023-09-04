930ff3adb3c9e2a00210bdc4a5c13e51174bd6d8
Gabriela Paulini Deutner <deutnerg@gmail.com>
2023-09-04 20:10:27 -0300

    Update Resol2_Equilibrando_Saldo.kt

--- 1_Equilibrando_Saldo/Resol2_Equilibrando_Saldo.kt
+++ 1_Equilibrando_Saldo/Resol2_Equilibrando_Saldo.kt
@@ -1,8 +1,17 @@
 fun main() {
-    val saldoAtual = readLine()!!.toFloat()
-    val valorDeposito = readLine()!!.toFloat()
-    val valorRetirada = readLine()!!.toFloat()
+    // Entrada de dados com tratamento de erros
+    val saldoAtual = readLine()?.toFloatOrNull()
+    val valorDeposito = readLine()?.toFloatOrNull()
+    val valorRetirada = readLine()?.toFloatOrNull()
 
-    val saldoFinal = (saldoAtual + valorDeposito) - valorRetirada
+    if (saldoAtual == null || valorDeposito == null || valorRetirada == null) {
+        println("Entrada inválida. Certifique-se de inserir números válidos.")
+        return
+    }
+
+    // Calcular o saldo final
+    val saldoFinal = saldoAtual + valorDeposito - valorRetirada
+
+    // Imprimir o saldo final com uma casa decimal
     println("Saldo atualizado na conta: %.1f".format(saldoFinal))
 }
