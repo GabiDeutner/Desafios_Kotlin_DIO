dbd625328fff278e07d2bcb8be1b7a79f7621b98
Gabriela Paulini Deutner <deutnerg@gmail.com>
2023-09-04 20:10:32 -0300

    Update Resol_Condicionalmente_Rico.kt

--- 3_Condicionalmente_Rico/Resol_Condicionalmente_Rico.kt
+++ 3_Condicionalmente_Rico/Resol_Condicionalmente_Rico.kt
@@ -1,13 +1,23 @@
 fun main() {
-  // Entrada de dados
-  val saldoTotal = readLine() !!.toInt()
-  val valorSaque = readLine() !!.toInt()
+    // Entrada de dados
+    val saldoTotal = readLine()?.toIntOrNull() ?: 0
+    val valorSaque = readLine()?.toIntOrNull() ?: 0
 
-  //TODO: Criar as condições necessárias para impressão da saída, vide tabela de exemplos.
-  if (saldoTotal >= valorSaque){
-    val saldoDisponivel = saldoTotal - valorSaque
-    print("Saque realizado com sucesso. Novo saldo: $saldoDisponivel")
-  } else {
-    print("Saldo insuficiente. Saque nao realizado!")
-  }
+    // Verificar se as entradas são válidas
+    if (saldoTotal <= 0 || valorSaque <= 0) {
+        println("Entrada inválida. Certifique-se de inserir números inteiros positivos.")
+    } else {
+        // Processar a transação
+        val saldoDisponivel = saldoTotal - valorSaque
+
+        // Imprimir o resultado com base nas condições
+        when {
+            saldoDisponivel >= 0 -> {
+                println("Saque realizado com sucesso. Novo saldo: $saldoDisponivel")
+            }
+            else -> {
+                println("Saldo insuficiente. Saque nao realizado!")
+            }
+        }
+    }
 }
